e97454d8b190b8769e51eb33b32e488fa2309879
Darshan Ponikar <darshanponikar@Darshan-Ponikars-MacBook-Pro.local>
2021-10-07 12:49:11 +0530

    Feature: todos filter

--- src/components/todos/todo-collection.jsx
+++ src/components/todos/todo-collection.jsx
@@ -1,20 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTodos } from "../../contexts/todos-context";
+import { todoFilters } from "./todo-filter-option.data";
 import { TodoItem } from "./todo-item";
 import "./todo.style.css";
 
 export const TodosCollection = () => {
   const { todos } = useTodos();
+  const [filter, setFilter] = useState("all");
+
+  const [filteredTodos, setFilterTodos] = useState(todos);
+
+  useEffect(() => {
+    switch (filter) {
+      case "non-urgent":
+        return setFilterTodos(() =>
+          todos.filter((todo) => todo.priority === "non-urgent")
+        );
+      case "neutral":
+        return setFilterTodos(() =>
+          todos.filter((todo) => todo.priority === "neutral")
+        );
+      case "completed":
+        return setFilterTodos(() => todos.filter((t) => t.completed));
+      case "urgent":
+        return setFilterTodos(() =>
+          todos.filter((todo) => todo.priority === "urgent")
+        );
+      default:
+        setFilterTodos(todos);
+    }
+  }, [filter, todos]);
+  useEffect(() => {}, [filter]);
   return !todos.length ? (
     <section className="no-todo-container">
       <img alt="No todos left to do!" src="/svgs/no-todos.svg" />
       <h2>Wooho! No Todos</h2>
     </section>
   ) : (
-    <ol style={{ width: "100%" }}>
-      {todos.map((t) => (
-        <TodoItem key={t.id} todo={t} />
-      ))}
-    </ol>
+    <>
+      <section className="filter-container">
+        {todoFilters.map(({ name, key }) => (
+          <div
+            key={key}
+            className={`${
+              key === filter ? "selected-filter" : "filter-not-selected"
+            }`}
+            onClick={(_) => setFilter(key)}
+          >
+            {" "}
+            {name}{" "}
+          </div>
+        ))}
+      </section>
+      <ol style={{ width: "100%" }}>
+        {filteredTodos.map((t) => (
+          <TodoItem key={t.id} todo={t} />
+        ))}
+      </ol>
+    </>
   );
 };
---
+++ src/components/todos/todo-filter-option.data.js
@@ -0,0 +1,22 @@
+export const todoFilters = [
+  {
+    name: "All",
+    key: "all",
+  },
+  {
+    name: "Completed",
+    key: "completed",
+  },
+  {
+    name: "Normal",
+    key: "neutral",
+  },
+  {
+    name: "Urgent",
+    key: "urgent",
+  },
+  {
+    name: "Non-Urgent",
+    key: "non-urgent",
+  },
+];
